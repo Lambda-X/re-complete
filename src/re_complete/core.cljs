087c883356a014ca5e7d7690b977b11adc58310d
Dajana Stiberova <dajana.stiberova@gmail.com>
2016-05-11 20:30:55 +0200

    change display when focus

--- src/re_complete/core.cljs
+++ src/re_complete/core.cljs
@@ -8,41 +8,41 @@
 (defn completion-list
   "Render list of the items to autocomplete.
   Every item of the list is dispatched to the right place in the right input with :on-click event."
-  ([linked-component-key onclick-callback]
-   (let [linked-component-keyword (keyword linked-component-key)
-         items-to-re-complete (subscribe [:get-items-to-complete linked-component-keyword])
-         current-word (subscribe [:get-previous-input linked-component-keyword])
-         selected-item (subscribe [:get-selected-item linked-component-keyword])
-         is-mouse-on-suggestion-list? (subscribe [:is-mouse-on-suggestion-list linked-component-keyword])
-         focus? (subscribe [:focus? linked-component-key])]
-     (fn []
-       (.log js/console (not @focus?))
-       (let [selected @selected-item]
-         (when (zero? (count @items-to-re-complete))
-           (dispatch [:clear-selected-item linked-component-keyword]))
-         [:ul {:className (str "re-completion-list " linked-component-key)
-               :style {:visibility (if (or (not @focus?) (empty? @items-to-re-complete)) "hidden" "visible")}}
-          (when-not (string/blank? @current-word)
-            (map (fn [item]
-                   (if (= (str (second selected))
-                          (str item))
-                     ^{:key item}
-                     [:li.re-completion-selected
-                      {:on-click #(do (dispatch [:add-completed-word linked-component-keyword item])
-                                      (when onclick-callback
-                                        (onclick-callback)))
-                       :on-mouse-out #(dispatch [:mouse-on-suggestion-list linked-component-key false])}
-                      item]
-                     ^{:key item}
-                     [:li.re-completion-item
-                      {:on-click #(do (dispatch [:add-completed-word linked-component-keyword item])
-                                      (when onclick-callback
-                                        (onclick-callback)))
-                       :on-mouse-over  #(when-not @is-mouse-on-suggestion-list?
-                                          (dispatch [:selected-item linked-component-keyword item]))
-                       :on-mouse-out #(dispatch [:mouse-on-suggestion-list linked-component-key false])}
-                      item]))
-                 @items-to-re-complete))])))))
+  [linked-component-key onclick-callback]
+  (let [linked-component-keyword (keyword linked-component-key)
+        items-to-re-complete (subscribe [:get-items-to-complete linked-component-keyword])
+        current-word (subscribe [:get-previous-input linked-component-keyword])
+        selected-item (subscribe [:get-selected-item linked-component-keyword])
+        is-mouse-on-suggestion-list? (subscribe [:is-mouse-on-suggestion-list linked-component-keyword])
+        focus? (subscribe [:focus? linked-component-key])]
+    (fn []
+      (let [selected @selected-item]
+        (when (zero? (count @items-to-re-complete))
+          (dispatch [:clear-selected-item linked-component-keyword]))
+        [:ul {:className (str "re-completion-list " linked-component-key)
+              :style {:visibility (if (empty? @items-to-re-complete) "hidden" "visible")
+                      :display (if @focus? "inline-block" "none")}}
+         (when-not (string/blank? @current-word)
+           (map (fn [item]
+                  (if (= (str (second selected))
+                         (str item))
+                    ^{:key item}
+                    [:li.re-completion-selected
+                     {:on-click #(do (dispatch [:add-completed-word linked-component-keyword item])
+                                     (when onclick-callback
+                                       (onclick-callback)))
+                      :on-mouse-out #(dispatch [:mouse-on-suggestion-list linked-component-key false])}
+                     item]
+                    ^{:key item}
+                    [:li.re-completion-item
+                     {:on-click #(do (dispatch [:add-completed-word linked-component-keyword item])
+                                     (when onclick-callback
+                                       (onclick-callback)))
+                      :on-mouse-over  #(when-not @is-mouse-on-suggestion-list?
+                                         (dispatch [:selected-item linked-component-keyword item]))
+                      :on-mouse-out #(dispatch [:mouse-on-suggestion-list linked-component-key false])}
+                     item]))
+                @items-to-re-complete))]))))
 
 (defn setup-key-handling [this linked-component-key onclick-callback]
   (let [node (reagent/dom-node this)
